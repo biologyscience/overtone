41ee4b93e137bf275d97e4c684745a30e3b23e5f
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2022-10-15 16:51:53 +0530

    visualizer sizings

---
+++ app/js/misc.js
@@ -0,0 +1,20 @@
+const audioVisualizer = document.getElementById('audioVisualizers');
+
+const bass = audioVisualizer.querySelector('.bass');
+const vocals = audioVisualizer.querySelector('.vocals');
+const highHats = audioVisualizer.querySelector('.highHats');
+
+const bassRange = bass.dataset.range.split('-').map(x => parseInt(x));
+const vocalsRange = vocals.dataset.range.split('-').map(x => parseInt(x));
+const highHatsRange = highHats.dataset.range.split('-').map(x => parseInt(x));
+
+const barWidth = parseInt(audioVisualizer.dataset.barWidth);
+const barGap = parseInt(audioVisualizer.dataset.barGap);
+
+const bassWidth = ((bassRange[1] - bassRange[0]) * barGap);
+const vocalsWidth = ((vocalsRange[1] - vocalsRange[0]) * barGap);
+const highHatsWidth = ((highHatsRange[1] - highHatsRange[0]) * barGap);
+
+bass.setAttribute('width', `${bassWidth}px`);
+vocals.setAttribute('width', `${vocalsWidth}px`);
+highHats.setAttribute('width', `${highHatsWidth}px`);
--- app/js/navbar/focus.js
+++ app/js/navbar/focus.js
@@ -42,8 +42,8 @@ function navbarStartup()
     };
 
     // const startUpHighlight = navbar.items[navbar.items.length - 1];
-    const startUpHighlight = navbar.items[1];
-    // const startUpHighlight = navbar.items[2];
+    // const startUpHighlight = navbar.items[1];
+    const startUpHighlight = navbar.items[6];
 
     navbar.div.style.setProperty('--left', `${math(startUpHighlight.offsetLeft)}px`);
 
