37203bd32f65e27cf279044a6f6fab08863f8ca4
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2023-02-27 14:10:07 +0530

    remove delays in hover3D

--- app/js/hover3D.js
+++ app/js/hover3D.js
@@ -1,21 +1,8 @@
 const hover3D = document.querySelectorAll('.hover3D');
 
-const object = {};
-
-function random()
-{
-    const number = Math.floor(Math.random() * 100);
-
-    if (object[number] !== undefined) random();
-
-    return number;
-};
-
 function move(E)
 {
     const target = E.path.reverse().splice(2).filter(x => x.classList.contains('hover3D'))[0];
-        
-    if (object[target.dataset.uid].hoverEffect === false) return;
 
     const { top, bottom, left, right } = target.getBoundingClientRect();
 
@@ -31,27 +18,8 @@ function move(E)
 
 hover3D.forEach((x) =>
 {
-    const rand = random();
-
-    x.dataset.uid = rand;
-
-    object[rand] = { hoverEffect: false, enteredWithinDelay: false };
-
     x.addEventListener('mouseout', () => x.style.transform = 'perspective(1000px) scale(1) rotateX(0) rotateY(0)');
     x.addEventListener('mousedown', () => x.style.transform = 'perspective(1000px) scale(1) rotateX(0) rotateY(0)');
     x.addEventListener('mouseup', () => x.style.transform = 'perspective(1000px) scale(1.025) rotateX(0) rotateY(0)');
     x.addEventListener('mousemove', move);
-
-    const { hoverEffect, enteredWithinDelay } = object[rand];
-
-    x.addEventListener('mouseenter', () =>
-    {
-        setTimeout(() => { object[rand].hoverEffect = true; }, 1000);
-
-        hoverEffect ? object[rand].enteredWithinDelay = true : null;
-    });
-
-    x.addEventListener('mouseleave', () => setTimeout(() => { enteredWithinDelay ? object[rand].enteredWithinDelay = false : object[rand].hoverEffect = false; }, 1000));
 });
-
-console.log(object)
