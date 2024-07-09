816646d3e294f0fe00fa9c47af4250fe7c8b9528
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2024-07-09 12:30:27 +0530

    heavy optimizations for search

--- app/js/search.js
+++ app/js/search.js
@@ -1,87 +1,43 @@
 function displayNone(x) { x.style.display = 'none'; };
 function displayRevert(x) { x.style.display = ''; };
 
-function searchInFolder(E)
-{
-    const input = E.target;
-    
-    const songListInFolder = document.getElementById('songListInFolder');
-
-    const children = Array.from(songListInFolder.children);
-
-    children.forEach(x => x.classList.remove('displayNone'));
-
-    const filtered = children.filter(x => !x.dataset.title.includes(input.value.toLowerCase()));
-
-    filtered.length === children.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
-    
-    filtered.forEach(x => x.classList.add('displayNone'));
-};
-
-function searchOutAlbum(E)
+function search(E)
 {
     const input = E.target;
 
-    const albumItems = Array.from(document.querySelector('section.album .out .body').children);
+    if (input.classList.contains('IgnoreForSearch')) return;
 
-    albumItems.forEach(x => x.classList.remove('displayNone'));
+    const
+        id = input.id,
+        inputValue = input.value.toLowerCase();
 
-    const filtered = albumItems.filter(x => !util.read.albums()[x.dataset.id].album.toLowerCase().includes(input.value.toLowerCase()));
+    const selector =
+    {
+        folderInput: '#songListInFolder',
+        outAlbumInput: 'section.album .out .body',
+        inAlbumInput: '#songListInAlbum',
+        outArtistInput: 'section.artist .out .body',
+        inArtistInput: '#albumListInArtist'
+    };
 
-    filtered.length === albumItems.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
+    const filterFunction =
+    {
+        folderInput: (x) => { return !x.dataset.title.includes(inputValue); },
+        outAlbumInput: (x) => { return !util.read.albums()[x.dataset.id].album.toLowerCase().includes(inputValue); },
+        inAlbumInput: (x) => { return !x.dataset.songName.toLowerCase().includes(inputValue); },
+        outArtistInput: (x) => { return !x.dataset.artist.toLowerCase().includes(inputValue); },
+        inArtistInput: (x) => { return !x.dataset.albumName.toLowerCase().includes(inputValue); }
+    };
     
-    filtered.forEach(x => x.classList.add('displayNone'));
-};
-        
-function searchInAlbum(E)
-{
-    const input = E.target;
+    const list = Array.from(document.querySelector(selector[id]).children);
 
-    const songList = Array.from(document.getElementById('songListInAlbum').children);
+    list.forEach(displayRevert);
 
-    songList.forEach(displayRevert);
+    const filtered = list.filter(filterFunction[id]);
 
-    console.log(songList);
-
-    const filtered = songList.filter(x => !x.dataset.songName.toLowerCase().includes(input.value.toLowerCase()));
-
-    filtered.length === songList.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
+    filtered.length === list.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
     
     filtered.forEach(displayNone);
 };
 
-function searchOutArtist(E)
-{
-    const input = E.target;
-
-    const artistItems = Array.from(document.querySelector('section.artist .out .body').children);
-
-    artistItems.forEach(x => x.classList.remove('displayNone'));
-
-    const filtered = artistItems.filter(x => !x.dataset.artist.toLowerCase().includes(input.value.toLowerCase()));
-
-    filtered.length === artistItems.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
-    
-    filtered.forEach(x => x.classList.add('displayNone'));
-};
-        
-function searchInArtist(E)
-{
-    const input = E.target;
-
-    const albumList = Array.from(document.getElementById('albumListInArtist').children);
-
-    albumList.forEach(displayRevert);
-
-    const filtered = albumList.filter(x => !x.dataset.albumName.toLowerCase().includes(input.value.toLowerCase()));
-
-    filtered.length === albumList.length ? input.classList.add('noMatch') : input.classList.remove('noMatch');
-    
-    filtered.forEach(displayNone);
-};
-
-document.getElementById('folderInput').addEventListener('input', searchInFolder);
-document.getElementById('outAlbumInput').addEventListener('input', searchOutAlbum);
-document.getElementById('inAlbumInput').addEventListener('input', searchInAlbum);
-document.getElementById('outArtistInput').addEventListener('input', searchOutArtist);
-document.getElementById('inArtistInput').addEventListener('input', searchInArtist);
+document.querySelectorAll('input').forEach(x => x.addEventListener('input', search));
