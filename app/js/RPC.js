dcb0243913112db59b931f86b5b7718017433f7b
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2024-12-01 14:36:11 +0530

    pause - play [rpc] timestamps

--- app/js/RPC.js
+++ app/js/RPC.js
@@ -7,9 +7,12 @@ const discordRPC =
 
 // Buttons will not appear on your discord app. But other users can see the buttons.
 // https://github.com/discordjs/RPC/issues/180#issuecomment-2313232518
+// Timestamps may also show similar behaviour.
 
 const pressence =
 {
+    startTimestamp: Date.now(),
+    endTimestamp: Date.now(),
     details: 'Name of the Song',
     state: 'Name of the Artist',
     largeImageText: 'Logo',
@@ -76,16 +79,20 @@ function updateRPC({detail})
 
     if (audio.paused)
     {
+        pressence.largeImageKey = images[md5];
         pressence.smallImageText = 'Paused';
         pressence.smallImageKey = 'pause';
-
-        pressence.largeImageKey = images[md5];
+        pressence.startTimestamp = Date.now() - (audio.currentTime * 1000);
+        pressence.endTimestamp = Date.now();
 
         discordRPC.client.setActivity(pressence);
     }
 
     else
     {
+        pressence.startTimestamp = Date.now() - (audio.currentTime * 1000);
+        pressence.endTimestamp = undefined;
+
         if (images[md5] === undefined)
         {
             fetch(`https://itunes.apple.com/search?media=music&entity=album&limit=5&term=${album} ${albumArtist}`).then(x => x.json()).then((x) =>
