70f63e4fec4366849776f5200643fd359daed361
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2024-12-03 16:48:57 +0530

    " "

--- app/js/artists/showAlbum.js
+++ app/js/artists/showAlbum.js
@@ -4,7 +4,7 @@ function showAlbum(E)
 
     const
         artistName = document.querySelector('section.artist .in .head .content .name').textContent,
-        target = document.querySelector(`section.album .out .body .albumItem[data-id=${util.formatter(albumName, artistName)}]`),
+        target = document.querySelector(`section.album .out .body .albumItem[data-id="${util.formatter(albumName, artistName)}"]`),
         offset = document.querySelector('section.album .out .head').offsetHeight + document.getElementById('titleBar').offsetHeight;
     
     document.querySelector('nav ul li[data-display-section="album"]').click();
